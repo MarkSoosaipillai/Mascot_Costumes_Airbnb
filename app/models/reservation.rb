54091a8bd6392acc9c57252e9696a8813f61c419
Mark Soosaipillai <markbrian14@hotmail.com>
2022-04-30 16:29:20 -0400

    fix

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,12 +1,12 @@
 class Reservation < ApplicationRecord
   belongs_to :user
   belongs_to :costume
-  validates :status, inclusion: {in: %w(pending approved rejected), default:"pending"}
-  validates :start_date, :end_date, :type => date
+  validates :status, inclusion: { in: %w(pending approved rejected), default: "pending" }
+  validates :start_date, :end_date #:type => date
   validates :start_date, :end_date # =>  after: => Time.now
-  validates :end_date => {after: :start_date}
-  validates :user_id, :costume_id, :status, :start_date, :end_date, presence:true
-  validates :message, :type => text
+  # validates :end_date => { after: :start_date }
+  validates :user_id, :costume_id, :status, :start_date, :end_date, presence: true
+  validates :message #:type => text
   # validates :dates_cannot_be_in_the_past
 
   # private
