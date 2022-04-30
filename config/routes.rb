f27f9956b15d2139792fccc88df37b92c0808ab3
Andrey Yuschenko <yuschenkoandrey.work@gmail.com>
2022-04-30 16:22:26 -0400

    Completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,18 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'costumes#index'
+
+  get '/costumes/:id', to: 'users#show'
+  get '/users/:user_id/costumes', to: 'users#list_by_user'
+  get '/costumes/new', to: 'users#new'
+
+  post '/costumes/', to: 'users#create'
+
+  get '/costumes/:id/edit', to: 'users#edit'
+
+  post '/costumes/:id/edit', to: 'users#update'
+
+  get '/my_costumes', to: 'users#my_costumes'
+
+  delete '/costumes/:id', to: 'users#destroy'
 end
