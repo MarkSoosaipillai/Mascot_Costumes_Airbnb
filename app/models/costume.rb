92b3ef6f7bda17e5f1a3f861d025a6554b1a8368
Jean-Francois Numainville <jfnumainville@gmail.com>
2022-04-30 12:35:06 -0400

    Adding partial validation to the models

--- app/models/costume.rb
+++ app/models/costume.rb
@@ -1,4 +1,8 @@
 class Costume < ApplicationRecord
   belongs_to :user
   has_many :reservations
+  validates :name, :descr, :price, :size, :category, :user_id, presence: true
+  validates :descr, length: {minimum: 20}
+  validates :size, inclusion: {in: %w(small medium large), message: "%{value} is not a valid size, choose small, medium or large"}
+  validates
 end
