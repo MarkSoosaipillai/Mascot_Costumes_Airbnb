3f6cee55e45ee0bce5641b369d9c94ebc2b0fec9
Andrey Yuschenko <yuschenkoandrey.work@gmail.com>
2022-05-01 18:02:05 -0400

    Seeds done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning up database..."
+Costume.destroy_all
+puts "Database cleaned"
+
+10.times do |i|
+    puts "Creating Costumes....#{i+1}"
+    user = User.create(
+        name: "Andrii",
+        email: "andrii@gmail.com",
+        password: "password"
+    )
+    costume = Costume.create(
+        name: "Barney Costume",
+        descr: "Nice costume for the best parties",
+        price: 29.99,
+        size: "small",
+        category: "Corporate",
+        user_id: 1,
+      )
+end
+puts "Costumes created"
