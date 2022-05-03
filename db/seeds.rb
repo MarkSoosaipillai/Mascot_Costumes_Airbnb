0ca7f06643033e0fad64d0da0d027975709e5d9a
Jean-Francois Numainville <jfnumainville@gmail.com>
2022-05-02 21:33:42 -0400

    Added faker gem + finished seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,51 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "open-uri"
+require "date"
+puts "Destroying previous data"
+Reservation.destroy_all
+Costume.destroy_all
+User.destroy_all
+
+15.times do
+    puts "Creating new users"
+     p new_user = User.new(name: Faker::Name.name, email: Faker::Internet.email, password: Faker::Internet.password(min_length: 8, max_length: 10))
+     new_user.save
+end
+
+#Retrieving the list of all user ids
+p user_ids_list = User.all.pluck(:id)
+
+
+puts "Creating new costumes"
+
+10.times do
+    p new_costume =  Costume.new(name: Faker::FunnyName.name, descr: Faker::Lorem.sentence(word_count: 10, supplemental: true), price: rand(30..100),
+                                  size: %w(small medium large).sample(1)[0], category: %w(Corporate Animal Sports Cartoon Wedding).sample(1)[0], user_id:  user_ids_list.sample(1)[0])
+    new_costume.save
+end
+
+#Retrieving the list of all mascot ids
+p mascot_ids_list = Costume.all.pluck(:id)
+
+puts "Creating new reservations"
+
+8.times do
+    today = Date.today
+
+    #The start date is chosen at random between today and the next 4 days, the end date is chosen at random between today + 5 and 9 days.
+    p new_reservation = Reservation.new(user_id:user_ids_list.sample(1)[0], costume_id: mascot_ids_list.sample(1)[0], status: %w(pending approved rejected).sample(1)[0],
+                                          start_date: today + rand(0..4), end_date: today + rand(5..9), message: Faker::Lorem.sentence(word_count: 3, supplemental: true) )
+
+      if new_reservation.valid? == false
+           puts new_reservation.errors.full_messages
+     end
+    new_reservation.save
+end
+
+
+# Use this code to return reason why object cannot be saved
+# if new_user.valid? == false
+#   puts new_user.errors.full_messages
+# end
