8ae1a7712f2ea9b642d41f34e3bb797a77a1776a
Suzanne Tordjman <suzannetor@gmail.com>
2022-05-03 15:14:10 -0400

    front-end changes on the homepage and index page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Creating costumes ..."
+
+Costume.create(
+  name: "Pokemon",
+  descr: "Nice costume, perfect state",
+  price: 25,
+  size: "medium",
+  category: "animal",
+  user_id: 2
+)
+
+puts "Finished!"
